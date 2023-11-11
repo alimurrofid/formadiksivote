8352cd509fd934f7d342614d3cd0dbfb6661bf1b
alimurrofid <alimurrofid77@gmail.com>
2023-11-11 11:06:24 +0700

    feat:layouting dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::prefix('dashboard')->group (function () {
+    Route::get('/', function () {
+        return view('dashboard.home');
+    })->name('dashboard.home');
+
+    Route::get('/kandidat', function () {
+        return view('dashboard.kandidat');
+    })->name('dashboard.kandidat');
+    Route::get('/user', function () {
+        return view('dashboard.user');
+    })->name('dashboard.user');
+    Route::get('/kandidat', function () {
+        return view('dashboard.kandidat');
+    })->name('dashboard.kandidat');
 });
