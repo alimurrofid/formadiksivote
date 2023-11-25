749b9a8892a4f37bcf2a240adc1d87dd274e28d9
alimurrofid <alimurrofid77@gmail.com>
2023-11-25 12:15:06 +0700

    feat:confetti

--- resources/views/user/countdown.blade.php
+++ resources/views/user/countdown.blade.php
@@ -19,8 +19,40 @@
             </div>
             <!-- End Card Candidates -->
         </div>
-        <a href="{{route('quickcount')}}" class="mt-4 text-lg font-semibold text-red-500 underline">Lihat Detail</a>
-
+        <a href="{{ route('quickcount') }}" class="mt-4 text-lg font-semibold text-red-500 underline">Lihat Detail</a>
     </div>
-
 @endsection
+@push('customJS')
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.1/dist/confetti.browser.min.js"></script>
+    <script>
+        // do this for 30 seconds
+        var duration = 30 * 1000;
+        var end = Date.now() + duration;
+
+        (function frame() {
+            // launch a few confetti from the left edge
+            confetti({
+                particleCount: 5,
+                angle: 60,
+                spread: 55,
+                origin: {
+                    x: 0
+                }
+            });
+            // and launch a few from the right edge
+            confetti({
+                particleCount: 5,
+                angle: 120,
+                spread: 55,
+                origin: {
+                    x: 1
+                }
+            });
+
+            // keep going until we are out of time
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+            }
+        }());
+    </script>
+@endpush
