b683142fa100a57df06a1ae5afad1efb3e85459b
alimurrofid <112758111+alimurrofid@users.noreply.github.com>
2024-10-27 20:42:30 +0700

    fix: sidebar dashboard name kandidat to candidate

--- resources/views/dashboard/partials/sidenav.blade.php
+++ resources/views/dashboard/partials/sidenav.blade.php
@@ -39,7 +39,7 @@
                         class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                         <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-circle-08"></i>
                     </div>
-                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Kandidat</span>
+                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Candidate</span>
                 </a>
             </li>
 
